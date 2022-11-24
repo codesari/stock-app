d9382307736f6e77dddb75488f75f6bd49efdc32
codesari <ahmtsr81@gmail.com>
2022-11-25 00:23:02 +0300

    stockSlice

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import stockReducer from "../features/stockSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import storage from "redux-persist/lib/storage/sessionActions
-Settings";
+
+// import storage from "redux-persist/lib/storage/session";
 //! default degeri local.session olarak degistirebiliriz
 //* setItem getItem ile ugrasmadan otomatik olarak storagelara atiyor redux.(hangi storage i seçtiysen)
 
@@ -26,6 +27,7 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    stock: stockReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
