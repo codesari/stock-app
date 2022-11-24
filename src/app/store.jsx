0d6e538b90143df95ed23c9166b4c6a7436c9d3c
codesari <ahmtsr81@gmail.com>
2022-11-24 23:53:36 +0300

    persistor

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage/session";
+import storage from "redux-persist/lib/storage";
+// import storage from "redux-persist/lib/storage/sessionActions
+Settings";
 //! default degeri local.session olarak degistirebiliriz
-import rootReducer from "./reducers";
+//* setItem getItem ile ugrasmadan otomatik olarak storagelara atiyor redux.(hangi storage i seçtiysen)
+
+//? serialize hatasini gidermek için middle ware ve ilgili importlari ekledik
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const persistConfig = {
   key: "root",
@@ -15,7 +27,14 @@ const store = configureStore({
   reducer: {
     auth: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
+
 export const persistor = persistStore(store);
 export default store;
