dcf0f1beaf94ad046df78ef62a496867b8bde554
codesari <ahmtsr81@gmail.com>
2022-11-24 23:33:26 +0300

    persistor

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -52,13 +52,22 @@ const icons = [
     url: "https://14268.fullstack.clarusway.com/admin",
   },
 ];
+
 //* yukaridaki url ler 2 çeşit.dahili url (relative path) lerde problem olmuyor direk url devaminda anlamli bir url yolu oluyor.
 //? harici url ler de (absolute path old.icin problem olusturuyor)
-//? harici linkler icin navigate kullanilamaz.navigate dahili calisir.onun yerine link to veya navlink to kullanmak gerekiyor
-//* navigate hook'u history'ye göre calisiyor.bulundugun yerden itibaren konum alkiyor.bu yüzden harici linklerde calismaz
-// ? bütün linkleri harici formata da cevirebilirdik fakat bu sefer bütün sayfalar refresh olurdu.react router'ı devre dışı bırakmış olurduk ve bu da dahili sayfalarda istemediğimiz birşey (sürekli refresh olmasi)
+//todo harici linkler icin navigate kullanilamaz.navigate dahili calisir.onun yerine link to veya navlink to kullanmak gerekiyor
+//! navigate hook'u history'ye göre calisiyor.bulundugun yerden itibaren konum alkiyor.bu yüzden harici linklerde calismaz
+//* bütün linkleri harici formata da cevirebilirdik fakat bu sefer bütün sayfalar refresh olurdu.react router'ı devre dışı bırakmış olurduk ve bu da dahili sayfalarda istemediğimiz birşey (sürekli refresh olmasi)
+//? link ve hover renkleri vermek gibi basit islemleri düz css ile yapabiliriz fakat bir kütüphane kullandiginda bunlari yapmak biraz daha zorlasiyor ezmek vs gerekiyor.
+
 const MenuListItems = () => {
   const navigate = useNavigate();
+
+  const listStyle = {
+    color: "#C2BB00",
+    "&:hover": { color: "#003547" },
+    "&:hover .MuiSvgIcon-root": { color: "#003547" },
+  };
 
   return (
     <List>
@@ -66,13 +75,13 @@ const MenuListItems = () => {
       {icons?.map((item, index) => (
         <ListItem key={index} disablePadding>
           {item.url.includes("http") && (
-            <ListItemButton to={item.url}>
+            <ListItemButton to={item.url} sx={listStyle}>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.title} />
             </ListItemButton>
           )}
           {!item.url.includes("http") && (
-            <ListItemButton onClick={() => navigate(item.url)}>
+            <ListItemButton onClick={() => navigate(item.url)} sx={listStyle}>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.title} />
             </ListItemButton>
@@ -84,3 +93,5 @@ const MenuListItems = () => {
 };
 
 export default MenuListItems;
+
+//* 2:12
